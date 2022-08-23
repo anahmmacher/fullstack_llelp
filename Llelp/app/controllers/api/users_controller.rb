a953f08b44ac2101d313f873016b7ad68ea5b7a5
anahmmacher <acnahmmacher@gmail.com>
2022-08-22 17:11:59 -0700

    removed turbolinks

---
+++ Llelp/app/controllers/api/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::UsersController < ApplicationController
+	def index
+		@users = User.all
+		render :index
+	end
+
+	def show
+		@user = User.find(params[:id])
+		if @user
+			render :show
+		else
+			render json: @user.errors.full_messages, status: 404
+		end
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			login!(@user)
+			render :show
+		else
+			render json: @user.errors.full_messages, status: 422
+		end
+	end
+
+	private
+	
+	def user_params
+		params.require(:user).permit(:email, :first_name, :last_name, :password)
+	end
+end
